2d96969bdeae337180db99865c6062535d7a2b5f
ahmed shalash <ashalash38@gmail.com>
2022-11-14 00:40:55 +0200

    add java 19 pro

---
+++ src/main/java/com/example/java19demo/Java19DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.java19demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java19DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java19DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java19demo/Java19DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java19demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java19DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
